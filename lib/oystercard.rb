b75a2b466ed2010dc88a7d685b65bf83aa8f8738
thomas <thomas.lubon@icloud.com>
2021-07-06 09:35:52 +0100

    added max amount of 90

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,4 +4,17 @@ class Oystercard
   def initialize
     @balance = 0
   end
+
+  def top_up(amount)
+    fail "maximum balance is 90" if max_value?(amount)
+    
+    @balance += amount
+  end
+
+  private
+
+  def max_value?(amount)
+    @balance + amount > 90
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,5 +6,22 @@ describe Oystercard do
       expect(subject.balance).to eq(0)
     end
   end
+
+  describe "#top_up" do
+    it "adds money to a new card" do
+      subject.top_up(10)
+      expect(subject.balance).to eq(10)
+    end
+
+    it "adds money correctly on multiple top_ups" do 
+      subject.top_up(10)
+      subject.top_up(10)
+      expect(subject.balance).to eq(20)
+    end
+
+    it "raises an error if balance is above 90" do
+      expect { subject.top_up(100) }.to raise_error "maximum balance is 90"
+    end
+  end
 end
 
