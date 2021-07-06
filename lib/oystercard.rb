6da2d58a683c1bbce7e1c74f1b1aa67556e73ee2
thomas <thomas.lubon@icloud.com>
2021-07-06 16:57:14 +0100

    added travel history

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,20 +1,44 @@
 class Oystercard
-  attr_reader :balance
+  CARD_LIMIT = 90
+  CARD_MINIMUM = 1  
+  attr_reader :balance, :entry_station, :exit_station, :journey_history
 
   def initialize
     @balance = 0
+    @journey_history =[]
   end
 
   def top_up(amount)
-    fail "maximum balance is 90" if max_value?(amount)
+    fail "maximum balance is £#{CARD_LIMIT}" if max_value?(amount)
     
     @balance += amount
+  end
+
+  def touch_in(station)
+    fail "minimum balance is #{CARD_MINIMUM}" if balance < CARD_MINIMUM 
+    @entry_station = station
+  end
+
+  def touch_out(station)
+    deduct(1)
+    @exit_station = station
+    journey_history.push( { entry_station: entry_station, exit_station: exit_station})
+    @entry_station = nil 
+  end
+
+  def in_journey?
+    !!entry_station
   end
 
   private
 
   def max_value?(amount)
-    @balance + amount > 90
+    @balance + amount > CARD_LIMIT
+  end
+
+  def deduct(amount)
+    @balance -= amount
   end
 
 end
+
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,6 +5,10 @@ describe Oystercard do
     it "new balance is zero" do
       expect(subject.balance).to eq(0)
     end
+
+    it "adds empty journey history" do
+      expect(subject.journey_history).to eq []
+    end
   end
 
   describe "#top_up" do
@@ -19,8 +23,83 @@ describe Oystercard do
       expect(subject.balance).to eq(20)
     end
 
-    it "raises an error if balance is above 90" do
-      expect { subject.top_up(100) }.to raise_error "maximum balance is 90"
+    it "raises an error if balance is above CARD_LIMIT" do
+      expect { subject.top_up(100) }.to raise_error "maximum balance is £#{Oystercard::CARD_LIMIT}"
+    end
+  end
+
+  describe "#deduct" do
+    xit "deducts money from a card" do
+      subject.top_up(10)
+      subject.deduct(5)
+      expect(subject.balance).to eq(5)
+    end
+  end
+
+  describe "if in use" do
+    let(:station) { double :station}
+    it "checks if user can touch" do
+      expect(subject).to respond_to(:touch_in)
+    end
+
+    it "checks if user has touched in" do
+      subject.top_up(10)
+      expect(subject.touch_in(station)).to eq(station)
+    end
+
+    it "raises an error if balance is below card minimum" do
+      expect {subject.touch_in(station)}.to raise_error "minimum balance is #{Oystercard::CARD_MINIMUM}"
+    end
+
+    it "is in use" do
+      subject.top_up(10)
+      subject.touch_in(station)
+      expect(subject.in_journey?).to eq(true)
+    end
+
+    it "checks if user can touch out" do
+      subject.top_up(10)
+      subject.touch_in(station)
+      expect(subject).to respond_to(:touch_out)
+    end
+
+    it "checks if user has touched out" do
+      subject.top_up(10)
+      subject.touch_in(station)
+      subject.touch_out(station)
+      expect(subject.in_journey?).to eq(false)
+    end
+
+
+
+    it "deducts £1 after the touch out" do
+      subject.top_up(10)
+      subject.touch_in(station)
+      expect {subject.touch_out(station)}.to change{subject.balance}.by(-1)
+    end
+  end
+
+  describe "saving journey info" do
+    let(:station1) { double :station1}
+    let(:station2) { double :station2}
+    it 'stores the entry station' do
+      subject.top_up(10)
+      subject.touch_in(station1)
+      expect(subject.entry_station).to eq station1
+    end
+    
+    it "stores the exit station" do 
+      subject.top_up(10)
+      subject.touch_in(station1)
+      subject.touch_out(station2)
+      expect(subject.exit_station).to eq station2
+    end
+
+    it "checks if a journey has been created" do
+      subject.top_up(10)
+      subject.touch_in(station1)
+      subject.touch_out(station2)
+      expect(subject.journey_history).to eq([{entry_station: station1, exit_station: station2}])
     end
   end
 end
